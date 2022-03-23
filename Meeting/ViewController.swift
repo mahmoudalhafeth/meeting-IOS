f488950bb7530d49974bd116cf1bad4f89cd480e
mahmoudalhafeth <101928983+mahmoudalhafeth@users.noreply.github.com>
2022-03-23 21:41:32 +0300

    Add files via upload

---
+++ Meeting/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Meeting
+//
+//  Created by Mahmoud Alhafeth on 22/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    // touch screen to hide the keyboard
+    //override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       // self.view.endEditing(true)
+    }
+    //func textFieldShouldReturn(_textField: UITextField) -> Bool {
+        //textField.canResignFirstResponder()
+       // return(true)
+    
+
+
+
